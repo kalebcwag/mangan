8912093714346b0344b918b0ea7f32a4c6f869ea
glenangelo <glen.angelo@ti.ukdw.ac.id>
2023-06-04 13:42:32 +0700

    create a kategori object and rerarrange the coding position

--- src/main/java/com/example/prototype/App.java
+++ src/main/java/com/example/prototype/App.java
@@ -17,24 +17,30 @@ public class App extends Application {
     public void start(Stage stage) throws Exception {
         app = this;
         App.stage = stage;
+        User kaleb = new User("kaleb", "kaleb");
+        User yandi = new User("yandi", "yandi");
+        User glen = new User("glen", "glen");
+
+        Database.addUser(kaleb);
+        Database.addUser(yandi);
+        Database.addUser(glen);
+
         RumahMakan[] rm = {new RumahMakan("Nasi Goreng Bang Rusdi", "0271", "Nasi", "Ngawi"),
         new RumahMakan("Nasi Cumi Hitam Pak Kris", "0821190889", "Nasi", "jl condong"),
         new RumahMakan("Mie Goreng Jawa", "08334677639", "Mie", "jl munggur"),
         new RumahMakan("Ayam Goreng", "0893948378499", "Snack", "jl kaliurang"),
         new RumahMakan("Pasta frizz", "08111908939", "Mie", "jl gatot")
         };
-
-        User kaleb = new User("kaleb", "kaleb");
-        User yandi = new User("yandi", "yandi");
-        User glen = new User("glen", "glen");
-
-        Database.add(kaleb);
-        Database.add(yandi);
-        Database.add(glen);
-
         for (RumahMakan r : rm) {
-            Database.add(r);
+            Database.addRm(r);
         }
+
+        Database.addCategory(new Kategori("Nasi"));
+        Database.addCategory(new Kategori("Mie"));
+        Database.addCategory(new Kategori("Ayam"));
+
+
+
         Parent root = FXMLLoader.load(getClass().getResource("halamanUtamaUser.fxml"));
         stage.setTitle("Halaman Utama");
 
